954e550f30edb9554206495b5fb74947bc59274b
Rahulshahare <rahulshahare@gmail.com>
2021-04-20 00:13:13 +0530

    logo added

--- src/App.js
+++ src/App.js
@@ -1,21 +1,22 @@
-import logo from './logo.svg';
+import logo2 from './oceangreenlogo.jpeg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <img src={logo2} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+          <code> Work in progress</code>
+          </p>
+          <p>Oeangreen Technology - We Work For Excellence</p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://oceangreen.ml"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Visit Site
         </a>
       </header>
     </div>
